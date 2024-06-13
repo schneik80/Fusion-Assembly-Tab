be318f6ac785ae8193dc385cea2b46603406e842
Kevin Schneider <schneik80@gmail.com>
2024-06-13 08:05:01 -0700

    update the tab tool install script

--- tabtoolbar/Tabtoolbar.py
+++ tabtoolbar/Tabtoolbar.py
@@ -1,29 +1,38 @@
 # Author-Kevin Schneider
 # Description-Install Assembly tab into Fusion design toolbar
 
-import adsk.core, adsk.fusion, adsk.cam, os, sys, traceback, pathlib, zipfile
+import adsk.core, adsk.fusion, adsk.cam, os, sys, traceback, pathlib, zipfile, json
 import urllib.request
 from sys import platform
 
 app = adsk.core.Application.get()
 ui = app.userInterface
+run = False  # track if the user clicks yes or no
+url = "https://raw.githubusercontent.com/schneik80/Fusion-Assembly-Tab/main/TabToolbars.xml"  # URL to the main branch in GITHUB for the new tabtoolbar.xml
 
+# Prompt user to run or exit
 ui.messageBox(
-    "Ready to install a new Design document Toolbar with a new Assembly Tab? ",
+    "Click YES, to install a new Design Document Toolbar with a dedicated Assembly Tab?\n\nA backup of your existing toolbar xml will be created automatically. ",
     "Install Assembly Tab",
     3,
     1,
 )
 
+# If we get here the user picked yes
+run = True
+
 
 def run(context):
     ui = None
-    try:
 
+    # Detect the OS platform so we can get the correct path to the tabtoolbar.xml
+    try:
+        # Windows
         if platform == "win32":
             winassytb()
             return
 
+        # Mac OS
         elif platform == "darwin":
             macassytb()
             return
@@ -34,24 +43,26 @@ def run(context):
 
 
 def close():
-    ui.messageBox(
-        "New Design toolbar with Assembly Tab is installed. Please save and close or close all open documents then restart Fusion",
-        "Install Assembly Tab",
-        0,
-        2,
-    )
+    if run == True:
+        ui.messageBox(
+            "New Design toolbar with Assembly Tab is installed. Please save and close or close all open documents then restart Fusion",
+            "Install Assembly Tab",
+            0,
+            2,
+        )
+    else:
+        exit
 
 
-def macassytb():
+# Windows
+def winassytb():
     try:
-        code_path = pathlib.Path(os.path.dirname(sys.executable))
-        print(f"MACOS {code_path}")
+        PATHS_DICT = json.loads(app.executeTextCommand("paths.get"))
+        code_path = pathlib.Path(PATHS_DICT.get("appDirectory"))
 
+        # set the path to the tabtoolbar.xml
         tb_path = os.path.join(
-            code_path.parent.parent.parent.parent.parent,
-            "Libraries",
-            "Applications",
-            "Fusion",
+            code_path.parent,
             "Fusion",
             "UI",
             "FusionUI",
@@ -60,11 +71,9 @@ def macassytb():
             "TabToolbars.xml",
         )
 
+        # set the path for the tabtoolbar.zip arrchive
         tb_zip = os.path.join(
-            code_path.parent.parent.parent.parent.parent,
-            "Libraries",
-            "Applications",
-            "Fusion",
+            code_path.parent,
             "Fusion",
             "UI",
             "FusionUI",
@@ -73,29 +82,33 @@ def macassytb():
             "TabToolbars.zip",
         )
 
+        # Check if the tabtoolbar.xml is there and zip it. This overwrites any existing zip.
         if os.path.exists(tb_path):
             zipfile.ZipFile(tb_zip, mode="w").write(tb_path)
         else:
             return
 
+        # Get the tabtoolbar.xml from the URL
         urllib.request.urlretrieve(
-            "https://raw.githubusercontent.com/schneik80/Fusion-Assembly-Tab/main/TabToolbars.xml",
+            url,
             tb_path,
         )
-
-        close()
 
     except:
         if ui:
             ui.messageBox("Failed:\n{}".format(traceback.format_exc()))
 
 
-def winassytb():
+# Mac OS
+def macassytb():
     try:
-        code_path = pathlib.Path(os.path.dirname(sys.executable))
+        PATHS_DICT = json.loads(app.executeTextCommand("paths.get"))
+        code_path = pathlib.Path(PATHS_DICT.get("appDirectory"))
 
+        # set the path to the tabtoolbar.xml
         tb_path = os.path.join(
-            code_path.parent,
+            code_path,
+            "Fusion",
             "Fusion",
             "UI",
             "FusionUI",
@@ -104,8 +117,10 @@ def winassytb():
             "TabToolbars.xml",
         )
 
+        # set the path for the tabtoolbar.zip archive
         tb_zip = os.path.join(
-            code_path.parent,
+            code_path,
+            "Fusion",
             "Fusion",
             "UI",
             "FusionUI",
@@ -114,19 +129,19 @@ def winassytb():
             "TabToolbars.zip",
         )
 
+        # Check if the tabtoolbar.xml is there and zip it. This overwrites any existing zip.
         if os.path.exists(tb_path):
             zipfile.ZipFile(tb_zip, mode="w").write(tb_path)
         else:
             return
 
+        # Get the tabtoolbar.xml from the URL
         urllib.request.urlretrieve(
-            "https://raw.githubusercontent.com/schneik80/Fusion-Assembly-Tab/main/TabToolbars.xml",
+            url,
             tb_path,
         )
 
         close()
-        # call to open the path in os file manager
-        # os.startfile(tb_path)
 
     except:
         if ui:
